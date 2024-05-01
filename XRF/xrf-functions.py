92f3e1e7799a59a3fabce63d602abc2a525d8ac8
Dominic Piedmont <144847957+drpiedmont@users.noreply.github.com>
2024-05-01 08:53:34 -0400

    Update xrf-functions.py

--- XRF/xrf-functions.py
+++ XRF/xrf-functions.py
@@ -1073,7 +1073,7 @@ def AOI_extractor(filename, min_energy, elements, AOI_x, AOI_y, BKG_x, BKG_y, pr
 
     ########## Identify elements ##########
     # identify fluorescent line energy that most closely matches the determined peaks
-    tolerance = 1 # allowed difference in percent
+    tolerance = 1.5 # allowed difference in percent
     matched_peaks, _ = identify_element_match(elements, energy_int[peaks]*1000, tolerance)
     # Plotting vertical lines for matched peaks and labeled with element symbol
     for i in range(len(matched_peaks)):
@@ -1301,7 +1301,7 @@ def standard_data_extractor(standard_filename, background_filename, open_air_fil
     
     ########## Find element of interest ##########
     # identify fluorescent line energy that most closely matches the determined peaks
-    tolerance = 1 # allowed difference in percent
+    tolerance = 1.5 # allowed difference in percent
     matched_peaks, _ = identify_element_match(element, energy_int[peaks]*1000, tolerance)
     
     # find peak belonging to element of interest
