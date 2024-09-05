f8b3e1d9ef2e99a1ee5ffb706b01444af2b25b5c
Dominic Piedmont <144847957+drpiedmont@users.noreply.github.com>
2024-09-05 10:33:59 -0400

    Update xrf-functions.py

--- XRF/xrf-functions.py
+++ XRF/xrf-functions.py
@@ -1324,8 +1324,12 @@ def AOI_extractor(filename, min_energy, elements, AOI_x, AOI_y, BKG_x, BKG_y, pr
         cax = divider.append_axes('right', size = '5%', pad = 0.05)
         
         ax.set_title('Energies in range: ' + str(round(energy[energy_ranges[i]].min(),3)) + '-' + str(round(energy[energy_ranges[i]].max(),3)) + ' keV')
-        ax.set_ylabel("y ($\mu$m)")
-        ax.set_xlabel("x ($\mu$m)")
+        if not Sigray:
+            ax.set_ylabel("y ($\mu$m)")
+            ax.set_xlabel("x ($\mu$m)")
+        if Sigray:
+            ax.set_ylabel("y (mm)")
+            ax.set_xlabel("x (mm)")
         im = ax.imshow(element_data, extent = [x_int.min(), x_int.max(), y_int.min(), y_int.max()], cmap='viridis')
         
         
